4587f5f3fa3a48f257f624bda1ef293f410daee4
maltymischief <spy2tech@yahoo.com>
2013-02-04 09:49:28 -0800

    Update traffic.py

--- traffic.py
+++ traffic.py
@@ -13,6 +13,25 @@ QUIT = (COMMAND_CHAR + "quit").lower()
 
 LOCAL_IP = socket.gethostbyname(socket.gethostname())	# Gets local IP address
 PORT = 7721												# Port to send packets on
+def connectionPrompt(connectionType):
+  
+	if connectionType[0:1] == 'L':
+		print "Using Local IP: " + socket.gethostbyname(socket.gethostname())
+		return socket.gethostbyname(socket.gethostname())
+		
+	elif connectionType[0:1] == 'R':
+		myIP = urllib2.urlopen('http://ip.42.pl/raw').read()
+		print "Using Public IP: " + myIP
+		return myIP
+		
+	elif connectionType[0:1] == 'M':
+		myIP = raw_input("IP Address: ")
+		myIP =str(myIP)
+		print "Using Manual IP: " + myIP
+		return myIP
+	else:
+		print 'INVALID INPUT'
+		connectionPrompt()
 
 def listenToSocket():
 	"""Get incoming messages."""
